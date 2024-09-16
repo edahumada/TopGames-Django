e566b57f061fe9b4be26a6bce3ac759d954c282d
Eduardo Ahumada <167267634+edahumada@users.noreply.github.com>
2024-09-16 19:25:27 -0300

    Update models.py
    se crean dos modelos: categoría y juego.

--- TOPGAMES/core/models.py
+++ TOPGAMES/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
+# Modelo para Cliente
 class Cliente(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     direccion = models.CharField(max_length=255, blank=True, null=True)
@@ -10,3 +10,23 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.usuario.username
+
+# Modelo para Categoria
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+# Modelo para Juego
+class Juego(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField(blank=True, null=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='juegos')
+    fecha_lanzamiento = models.DateField()
+    imagen = models.ImageField(upload_to='juegos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
