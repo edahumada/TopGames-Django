e0550c15f81eb2b8cc39e1b081a4b4c337bfe741
Eduardo Ahumada <167267634+edahumada@users.noreply.github.com>
2024-09-16 20:05:52 -0300

    Update urls.py
    Se agregan los path para la recuperación de contraseña.

--- TOPGAMES/core/urls.py
+++ TOPGAMES/core/urls.py
@@ -7,3 +7,10 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('carrito/', carrito, name='carrito'),
     path('modificar_perfil/', modificar_perfil, name='modificar_perfil'),
+    
+    #recuperación de contraseña
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='core/password_reset.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='core/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='core/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='core/password_reset_complete.html'), name='password_reset_complete'),
+]
