13a179a7c721f310fd6d6beaa378c1a9b2677a4b
Eduardo Ahumada <167267634+edahumada@users.noreply.github.com>
2024-09-16 20:00:15 -0300

    Update urls.py

--- TOPGAMES/core/urls.py
+++ TOPGAMES/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, login, carrito
+from django.contrib.auth import views as auth_views
+from .views import index, login, carrito, modificar_perfil
 
 urlpatterns = [
     path('', index, name='index'),
     path('login/', login, name='login'),
     path('carrito/', carrito, name='carrito'),
-]
+    path('modificar_perfil/', modificar_perfil, name='modificar_perfil'),
