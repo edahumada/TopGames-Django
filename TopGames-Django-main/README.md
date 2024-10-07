d520e4683c0b9a3bc0f988a472f8b5a8fdbfb7da
Eduardo Ahumada <167267634+edahumada@users.noreply.github.com>
2024-10-06 21:32:21 -0300

    Delete TopGames-Django-main directory

--- TopGames-Django-main/README.md
+++
@@ -1,3 +0,0 @@
-# TopGames-Django
-[TopGames migrado al framework Django para mejorar la gestión de vistas y la estructura del proyecto.](https://github.com/edahumada/TopGames-Django)
-
--- TopGames-Django-main/TOPGAMES/rest_api/TOPGAMES/permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import permissions
-
-class IsAuthenticatedAndActive(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.is_active)
